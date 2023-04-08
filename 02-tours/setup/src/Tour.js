28136072034f7df8b6b252ab5d44b5021f4373bd
Kiyarash <kiarashnadri@yahoo.com>
2023-04-08 13:27:27 +0330

    43 min

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ ...tour }) => {
+  const [readMore, setReadMore] = useState(false);
+  console.log(tour)
+  return <article className='single-tour'>
+    <img src={tour.image} alt={tour.name} />
+    <footer>
+      <div className="tour-info">
+        <h4>{tour.name}</h4>
+        <h4 className='tour-price'>${tour.price}</h4>
+      </div>
+      <p>
+        {readMore ? tour.info : `${tour.info.substring(0, tour.info.length / 2)}...`}
+        <button onClick={() => setReadMore(!readMore)}>
+          {readMore ? 'show less' : 'read more'}
+        </button>
+      </p>
+      <button className='delete-btn'>
+        not interested
+      </button>
+    </footer>
+  </article>;
 };
 
 export default Tour;
--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = (props) => {
+  const tours = props.tours;
+  return <section>
+    <div className="title">
+      <h2>our tours</h2>
+      <div className="underline"></div>
+    </div>
+    <div>
+      {tours.map((tour) => {
+        return <Tour key={tour.id} {...tour} />
+      })}
+    </div>
+  </section>;
 };
 
 export default Tours;
