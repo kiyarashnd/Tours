46e88e4f3b5b840d01e655046f5428161e94f49d
Kiyarash <kiarashnadri@yahoo.com>
2023-04-12 20:04:36 +0330

    add a svg shopping cart

--- 02-tours/setup/src/Cart.js
+++ 02-tours/setup/src/Cart.js
@@ -5,21 +5,21 @@ const Cart = ({ newTours2 }) => {
     const persian = Intl.NumberFormat("fa")
     console.log(persian.format(2312));
     return (
-        <footer className='main-footer'>
+        <section className='main-footer'>
             {newTours2.map((tour) => {
                 let price = Number(tour.price.replaceAll(',', ""))
                 sum += price;
                 return (
                     <div key={tour.id}>
                         <p className='para'>
-                            {tour.name}
+                            {tour.name} is {tour.price}$
                         </p>
                     </div>
                 )
             })}
             <p>total price is {Intl.NumberFormat().format(sum)}</p>
             <Link to="/">Back to home</Link>
-        </footer>
+        </section>
     );
 }
 
