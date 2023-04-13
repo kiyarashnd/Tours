cddf259bc16ee46dd21441813d884a280f3f6084
Kiyarash <kiarashnadri@yahoo.com>
2023-04-13 14:23:47 +0330

    add style to Cart component

--- 02-tours/setup/src/Cart.js
+++ 02-tours/setup/src/Cart.js
@@ -5,20 +5,18 @@ const Cart = ({ newTours2 }) => {
     const persian = Intl.NumberFormat("fa")
     console.log(persian.format(2312));
     return (
-        <section className='main-footer'>
+        <section className='items'>
             {newTours2.map((tour) => {
                 let price = Number(tour.price.replaceAll(',', ""))
                 sum += price;
                 return (
-                    <div key={tour.id}>
-                        <p className='para'>
-                            {tour.name} is {tour.price}$
-                        </p>
-                    </div>
+                    <p key={tour.id} className='liClass'>
+                        - {tour.name} is <span>{tour.price}$</span>
+                    </p>
                 )
             })}
-            <p>total price is {Intl.NumberFormat().format(sum)}</p>
-            <Link to="/">Back to home</Link>
+            <p className='totalPrice'>total price is {Intl.NumberFormat().format(sum)} $</p>
+            <Link to="/" className="backHome">Back to home</Link>
         </section>
     );
 }
