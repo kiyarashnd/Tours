672f659af7f4f41f92bd5047a5a181464953dab4
Kiyarash <kiarashnadri@yahoo.com>
2023-04-14 20:38:35 +0330

    1h & 5min

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -3,7 +3,30 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, useIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+  return <article className='review'>
+    <div className="img-container">
+      <img src={image} alt={name} className='person-img' />
+      <span className='quote-icon'>
+        <FaQuoteRight />
+      </span>
+    </div>
+    <h4 className='author'>{name}</h4>
+    <p className='job'>{job}</p>
+    <p className='info'>{text}</p>
+    <div className='button-container'>
+      <button className='prev-btn'>
+        <FaChevronLeft />
+      </button>
+      <button className='next-btn'>
+        <FaChevronRight />
+      </button>
+    </div>
+    <button className='random-btn'>
+      suprise me
+    </button>
+  </article>;
 };
 
 export default Review;
