8b3e0a828af82cb18c2d9379057ceb021876fbfc
Kiyarash <kiarashnadri@yahoo.com>
2023-04-14 19:42:15 +0330

    delete icon

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import Review from './Review';
-import { FaGithubAlt } from 'react-icons/fa'
 function App() {
-  return <h2>
-    <FaGithubAlt className='icon' />
-    reviews project setup</h2>;
+  return <h2>reviews project setup</h2>;
 }
 
 export default App;
