3555c1e6b914e9f17b2ca454bc3367fae92cba57
Kiyarash <kiarashnadri@yahoo.com>
2023-04-18 13:43:40 +0330

    read code of Review project

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -1,8 +1,9 @@
-import React, { useInsertionEffect, useState } from 'react';
+import React, { useState } from 'react';
 import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
+  //this initial value run just in initial rendering
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
