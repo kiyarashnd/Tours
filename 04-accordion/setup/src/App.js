22af5acd23fef068f4d888c92b66de35902da628
Kiyarash <kiarashnadri@yahoo.com>
2023-04-27 23:16:48 +0330

    1h and 25 min

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,9 +1,21 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
+import Question from './Question';
 function App() {
   const [questions, setQuestions] = useState(data);
-  return <main>accordion project setup</main>;
+  return <main>
+    <div className="container">
+      <h3>questions and answers and about login</h3>
+      <section className="info">
+        {
+          questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />
+          })
+        }
+      </section>
+    </div>
+  </main>;
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ title, info }) => {
+  return <article className='question'>
+    <header>
+      <h4>{title}</h4>
+      <button className='btn'>btn</button>
+    </header>
+    <p>{info}</p>
+  </article>;
 };
 
 export default Question;
