5f828e1e66eae4646a84844bd39a4a2d229d300f
Kiyarash <kiarashnadri@yahoo.com>
2023-05-01 13:28:00 +0330

    1h and 41 min

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -6,13 +6,19 @@ import items from './data';
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
+
+  const filterItems = (category) => {
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems)
+  }
+
   return <main>
     <section className='menu section'>
       <div className="title">
         <h2>our menu</h2>
         <div className="underline"></div>
       </div>
-      <Categories />
+      <Categories filterItems={filterItems} />
       <Menu items={menuItems} />
     </section>
   </main>;
