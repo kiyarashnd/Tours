806d4f9fd06ac1849312832ec1648adae0677e54
Kiyarash <kiarashnadri@yahoo.com>
2023-05-01 21:42:47 +0330

    finish menu project 1h,52min and add .prettierrc file

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -1,27 +1,42 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from 'react'
+import Menu from './Menu'
+import Categories from './Categories'
+import items from './data'
+
+//for be sync with our data when we want add a new item with new category and no need to add button manually
+const allCategories = ['all', ...new Set(items.map((item) => item.category))]
 
 function App() {
-  const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories] = useState(allCategories)
+  const [selectedCategory, setSelectedCategory] = useState('all')
 
   const filterItems = (category) => {
-    const newItems = items.filter((item) => item.category === category);
+    setSelectedCategory(category)
+    if (category === 'all') {
+      setMenuItems(items)
+      return
+    }
+    const newItems = items.filter((item) => item.category === category)
     setMenuItems(newItems)
   }
 
-  return <main>
-    <section className='menu section'>
-      <div className="title">
-        <h2>our menu</h2>
-        <div className="underline"></div>
-      </div>
-      <Categories filterItems={filterItems} />
-      <Menu items={menuItems} />
-    </section>
-  </main>;
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories
+          categories={categories}
+          filterItems={filterItems}
+          selectedCategory={selectedCategory}
+        />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const Categories = ({ filterItems }) => {
-  return <div className='btn-container'>
-    <button className='filter-btn' onClick={() => filterItems('breakfast')}>
-      breakfast
-    </button>
-  </div>;
+const Categories = ({ categories, filterItems, selectedCategory }) => {
+  //filterItems is a function
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            className={`filter-btn ${
+              category === selectedCategory ? "active" : undefined
+            }`}
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Categories;
