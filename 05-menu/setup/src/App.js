90d64b63d655cc8258272f95d7a22cf570a26a00
Kiyarash <kiarashnadri@yahoo.com>
2023-05-01 13:15:02 +0330

    make components and iterate over components

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -5,7 +5,17 @@ import items from './data';
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  return <h2>menu project setup</h2>;
+  const [categories, setCategories] = useState([]);
+  return <main>
+    <section className='menu section'>
+      <div className="title">
+        <h2>our menu</h2>
+        <div className="underline"></div>
+      </div>
+      <Categories />
+      <Menu items={menuItems} />
+    </section>
+  </main>;
 }
 
 export default App;
