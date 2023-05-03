55365f7dd0180bb6b62146a88d1208b81434a4a4
Kiyarash <kiarashnadri@yahoo.com>
2023-05-03 23:58:21 +0330

    2h and 2min of Tabs project

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -5,7 +5,31 @@ import { FaAngleDoubleRight } from "react-icons/fa";
 const url = "https://course-api.com/react-tabs-project";
 function App() {
   const [loading, setLoading] = useState(true);
-  return <h2>tabs project setup</h2>;
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>loading...</h1>
+      </section>
+    );
+  }
+  const { company, dates, duties, title } = jobs[value];
+
+  return <h2>jobs</h2>;
 }
 
 export default App;
