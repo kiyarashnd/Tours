1bb360ff79c43ca1887ee898a5ab87f63439eda5
Kiyarash <kiarashnadri@yahoo.com>
2023-05-10 16:29:18 +0330

    for see how slider work in fact

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -17,6 +17,15 @@ function App() {
           const { id, image, name, title, quote } = person;
           //more stuff coming up
           let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
           return (
             <article className={position} key={id}>
               <img src={image} alt={name} className="person-img" />
@@ -27,10 +36,10 @@ function App() {
             </article>
           );
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
