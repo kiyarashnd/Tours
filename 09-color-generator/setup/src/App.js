ff1b5a55df37d7899feda42c947386b42c64b074
Kiyarash <kiarashnadri@yahoo.com>
2023-05-15 00:26:02 +0330

    initial commit for Color-Generator project

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+
+  return <h2>color generator project</h2>;
 }
 
-export default App
+export default App;
