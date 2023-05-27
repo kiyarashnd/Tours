fb1ab9083f815daf107b5369f16a73d9379d7dfc
Kiyarash <kiarashnadri@yahoo.com>
2023-05-27 10:11:32 +0330

    initial commit for Grocery-Bud project

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,9 +1,14 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState("");
+  const [list, setList] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  return <h2>grocery bud setup</h2>;
 }
 
-export default App
+export default App;
