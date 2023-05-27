e365083039d5d3bf5cf0e4f914a2dccc57265a63
Kiyarash <kiarashnadri@yahoo.com>
2023-05-27 12:04:46 +0330

    save items in list as an array and show in List component

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -4,7 +4,7 @@ import Alert from "./Alert";
 
 function App() {
   const [name, setName] = useState("");
-  const [list, setList] = useState("");
+  const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
@@ -17,7 +17,9 @@ function App() {
       //deal with eidt
     } else {
       //show alert
-      const newItem = { id: new Date().getTime().toString() };
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
     }
   };
   return (
@@ -38,10 +40,12 @@ function App() {
           </button>
         </div>
       </form>
-      <div className="grocery-container">
-        <List />
-        <button className="clear-btn">clear items</button>
-      </div>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">clear items</button>
+        </div>
+      )}
     </section>
   );
 }
