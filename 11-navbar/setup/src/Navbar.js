2018433c939c1704755e893c0eb16cdf803453a8
Kiyarash <kiarashnadri@yahoo.com>
2023-06-03 01:07:26 +0330

    5h and make li list dynamic

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -15,36 +15,25 @@ const Navbar = () => {
         </div>
         <div className='links-container show-container'>
           <ul className='links'>
-            <li>
-              <a href='#'>home</a>
-            </li>
-            <li>
-              <a href='#'>about</a>
-            </li>
-            <li>
-              <a href='#'>contact</a>
-            </li>
-            <li>
-              <a href='#'>products</a>
-            </li>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
           </ul>
         </div>
         <ul className='social-icons'>
-          <li>
-            <a href='https://www.twitter.com'>
-              <FaTwitter />
-            </a>
-          </li>
-          <li>
-            <a href='https://www.twitter.com'>
-              <FaTwitter />
-            </a>
-          </li>
-          <li>
-            <a href='https://www.twitter.com'>
-              <FaTwitter />
-            </a>
-          </li>
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
