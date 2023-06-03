169e9c8c60dd09b3c963cf6343436d907522c8d6
Kiyarash <kiarashnadri@yahoo.com>
2023-06-03 14:11:49 +0330

    change toggle with transition

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -17,20 +17,24 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        {showLinks && (
-          <div className='links-container show-container'>
-            <ul className='links'>
-              {links.map((link) => {
-                const { id, url, text } = link;
-                return (
-                  <li key={id}>
-                    <a href={url}>{text}</a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
+        {/* {showLinks && ( */}
+        <div
+          className={`${
+            showLinks ? 'links-container show-container ' : 'links-container'
+          }`}
+        >
+          <ul className='links'>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        {/* )} */}
         <ul className='social-icons'>
           {social.map((socialIcon) => {
             const { id, url, icon } = socialIcon;
