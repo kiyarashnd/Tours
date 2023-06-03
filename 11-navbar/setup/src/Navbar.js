dad696f65dda43017ea7a0a425488c3595694e66
Kiyarash <kiarashnadri@yahoo.com>
2023-06-03 14:39:14 +0330

    useRef for set height of navbar in small size dynamic 5h 16 min finish project

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -5,6 +5,19 @@ import logo from './logo.svg';
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    console.log(linksHeight);
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  }, [showLinks]);
+
   return (
     <nav>
       <div className='nav-center'>
@@ -18,12 +31,8 @@ const Navbar = () => {
           </button>
         </div>
         {/* {showLinks && ( */}
-        <div
-          className={`${
-            showLinks ? 'links-container show-container ' : 'links-container'
-          }`}
-        >
-          <ul className='links'>
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
