6efd7e7230bf73f28977b3e71c3689514aeb94f5
Kiyarash <kiarashnadri@yahoo.com>
2023-06-08 18:30:40 +0330

    Review useRef

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { FaBars, FaTwitter } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa';
 import { links, social } from './data';
 import logo from './logo.svg';
 
@@ -8,21 +8,8 @@ const Navbar = () => {
   const linksContainerRef = useRef(null);
   const linksRef = useRef(null);
 
-  let count = useRef(0); //this line is read just in initial render
-  console.log(count);
-
-  count.current = count.current + 1;
-  console.log('new count : ', count.current); //on every re render this gonna change
-
-  let countVariable = 0;
-  console.log(countVariable);
-
-  countVariable = countVariable + 1;
-  console.log('new count variable : ', countVariable); //in every re render this not change and is still 1
-
   useEffect(() => {
     const linksHeight = linksRef.current.getBoundingClientRect().height;
-    console.log(linksHeight);
     if (showLinks) {
       linksContainerRef.current.style.height = `${linksHeight}px`;
     } else {
