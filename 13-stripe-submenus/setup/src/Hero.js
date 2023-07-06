e31806f98f5549c432ed06310b7fe50646993529
Kiyarash <kiarashnadri@yahoo.com>
2023-07-06 12:26:05 +0330

    work on Stripe Sidebar 6h and 14min

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -3,9 +3,24 @@ import phoneImg from './images/phone.svg';
 import { useGlobalContext } from './context';
 
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log(data);
-  return <h2>hero component</h2>;
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className='hero'>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>Manage Any Content Anywhere</h1>
+          <p>
+            Strapi is the leading open-source headless CMS. It's 100% JavaScript
+            and fully customizable.
+          </p>
+          <button className='btn'>Start now</button>
+        </article>
+        <article className='hero-images'>
+          <img src={phoneImg} className='phone-img' alt='phone' />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
